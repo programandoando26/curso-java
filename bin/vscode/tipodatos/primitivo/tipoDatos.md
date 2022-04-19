194405da4caa597600de032783bbaf06fe3b7c0b
ruben lopez <lopezsalasrubendario@gmail.com>
2022-04-18 23:37:03 -0500

    1 Tipo de datos

---
+++ bin/vscode/tipodatos/primitivo/tipoDatos.md
@@ -0,0 +1,13 @@
+## TIPOS DE DATOS:
+
+los tipos de datos o ` “tipos de datos primitivos”` son variables que no necesitan una invocación para ser creados, ósea que no son objetos.
+
+- ` ESTOS TIPOS DE DATOS SON`:
+  > ("[bytes]");
+  > ("[short]");
+  > ("[int]");
+  > ("[long]");
+  > ("[float]");
+  > ("[doule]");
+  > ("[char]");
+  > ("[boolean]");
---
+++ src/vscode/tipodatos/primitivo/TipoDatos.java
@@ -0,0 +1,23 @@
+package vscode.tipodatos.primitivo;
+
+public class TipoDatos {
+    public static void main(String[] args) {
+        System.out.println("");
+        // #red
+        System.out.println("Estos son los tipos de datos");
+        System.out.println("[bytes] ");
+        System.out.println("[short] ");
+        System.out.println("[int] ");
+        System.out.println("[long] ");
+        System.out.println("[float] ");
+        System.out.println("[double] ");
+        System.out.println("[char] ");
+        System.out.println("[boolean] ");
+        // #
+
+        /**
+         * ! ver el archivo tipoDatos.md
+         * ? en el se encuentra la explicacion
+         */
+    }
+}
